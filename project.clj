8cf617d885fbad4994326caadcd23a84efbf78d2
khmelevskii <y@uwinart.com>
2021-04-01 22:47:50 -0700

    Bump version 0.1.4

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject emotion-cljs "0.1.3"
+(defproject emotion-cljs "0.1.4"
   :description "ClojureScript wrapper for Emotion."
   :url "https://github.com/khmelevskii/emotion-cljs"
   :license {:name "Eclipse Public License"
