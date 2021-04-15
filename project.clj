d9cbd3592364693cd4bfe492803a310ba4dbdd2d
khmelevskii <y@uwinart.com>
2021-04-14 20:35:00 -0700

    Add cljs-bean dependency

--- project.clj
+++ project.clj
@@ -2,5 +2,6 @@
   :description "ClojureScript wrapper for Emotion."
   :url "https://github.com/khmelevskii/emotion-cljs"
   :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v20.html"}
-  :source-paths ["src" "test"])
+            :url  "http://www.eclipse.org/legal/epl-v20.html"}
+  :source-paths ["src" "test"]
+  :dependencies [[cljs-bean "1.6.0"]])
